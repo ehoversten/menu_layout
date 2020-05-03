66a9188d373945bde5ff64988a6efb5c52b02a78
ehoversten <ehoversten@gmail.com>
2020-05-03 15:33:26 -0400

    starting menu item objects for html population

---
+++ static/js/dinner_items.js
@@ -0,0 +1,162 @@
+
+// -- SHARE PLATES COLD ITEMS -- //
+const dinner_items = [
+    {
+        item_name: "Jonjoli Salad",
+        item_price: 7,
+        ingredients: [],
+        description: "Jonjoli, fresh and house-pickled cucumber, onion, green tomato, fresh herbs and sunflower",
+        section: "share-cold"
+    },
+    {
+        item_name: "Tiflis Salad",
+        item_price: 10,
+        ingredients: [],
+        description: "Salad greens, cucumber, tomato, crumbled guda cheese, fresno pepper, lemon-tarragon dressing",
+        section: "share-cold"
+    },
+    {
+        item_name: "Pkhali Assorti",
+        item_price: 12,
+        ingredients: [],
+        description: "Seasonal pates of vegetables minced with walnuts, garlic, and herbs, served with house-made breads",
+        section: "share-cold"
+    },
+    {
+        item_name: "Pickle Assorti",
+        item_price: 7,
+        ingredients: [],
+        description: "House-pickled green tomato, red cabbage, turkish peppers, cucumbers",
+        section: "share-cold"
+    },
+    {
+        item_name: "Kutaisi Salad",
+        item_price: 12,
+        ingredients: [],
+        description: "Tomato, cucumber, basil, red onion, cubanelle pepper, walnut dressing",
+        section: "share-cold"
+    },
+    {
+        item_name: "House Spreads",
+        item_price: [5, 12],
+        ingredients: [],
+        description: "Soft cheese 'nadughi', roasted pepper 'bulgaruli', roasted eggplant 'badrijani', house-made breads",
+        section: "share-cold"
+    },
+    {
+        item_name: "Eggplant Nigvzit",
+        item_price: 8,
+        ingredients: [],
+        description: "eggplant rolls stuffed with walnut-garlic paste, coriander, blud fenugreek, cilantro",
+        section: "share-cold"
+    },
+    {
+        item_name: "Beets Tkemalit",
+        item_price: 12,
+        ingredients: [],
+        description: "Salt-roasted beets, grilled fennel, toasted hazelnuts, georgian-spiced yougurt and red tkemali vinaigrette",
+        section: "share-cold"
+    },
+    {
+        item_name: "Supra Tasting Board",
+        item_price: 25,
+        ingredients: [],
+        description: "Seasonal vegetable pate pkhali, house-made pickles, eggplant nigvzit, bulgaruli and badrijani spreads, georgian cheese, house-made breads",
+        section: "share-cold"
+    },
+];
+
+// -- SHARE PLATES HOT ITEMS -- //
+const dinner_items_hot = [
+    {
+        item_name: "Chvishtari",
+        item_price: 7,
+        ingredients: [],
+        description: "Fritters of corn and smoked sulguni, lemon-mint yogurt sauce, red ajaka",
+        section: "share-hot"
+    },
+    {
+        item_name: "Ghomi",
+        item_price: 10,
+        ingredients: [],
+        description: "White cornmeal, smoked sulguni",
+        section: "share-hot"
+    },
+    {
+        item_name: "Mussels Pomodorit",
+        item_price: 12,
+        ingredients: [],
+        description: "P.E.I mussels, broth of tomato and fresh herbs, crumble of fried mchadi, walnut and lemon zest",
+        section: "share-hot"
+    },
+    {
+        item_name: "Potatoes Ojakhuri",
+        item_price: 7,
+        ingredients: [],
+        description: "Gold potatoes, utskho suneli spice, caramelized onion",
+        section: "share-hot"
+    },
+    {
+        item_name: "Lobio Kotanshi",
+        item_price: 10,
+        ingredients: [],
+        description: "Red beans stewed with traditional spices, house-made pickles, cornbread mchadi",
+        section: "share-hot"
+    },
+    {
+        item_name: "Lobio Kotanshi",
+        item_price: 10,
+        ingredients: [],
+        description: "Red beans stewed with traditional spices, house-made pickles, cornbread mchadi",
+        section: "share-hot"
+    },
+    {
+        item_name: "Lamb Chakapuli",
+        item_price: 14,
+        ingredients: [],
+        description: "Lamb in broth of sour plum tkemali, white wine, tarragon and other fresh herbs",
+        section: "share-hot"
+    },
+    {
+        item_name: "Soko Ketse",
+        item_price: 10,
+        ingredients: [],
+        description: "Cremini mushrooms stuffed with house-made sulguni cheese",
+        section: "share-hot"
+    },
+    {
+        item_name: "Chkhirtma",
+        item_price: 10,
+        ingredients: [],
+        description: "Sliky chicken soup, cilantro, black pepper",
+        section: "share-hot"
+    },
+    {
+        item_name: "Pork Neknebi",
+        item_price: 14,
+        ingredients: [],
+        description: "House-smoked baby back ribs, ajika glaze, pickled cabbage slaw",
+        section: "share-hot"
+    },
+    {
+        item_name: "Cauliflower Satsivi",
+        item_price: 10,
+        ingredients: [],
+        description: "Roasted cauliflower, walnut sauce, fresh herbs",
+        section: "share-hot"
+    },
+    {
+        item_name: "Megruli Kharacho",
+        item_price: 16,
+        ingredients: [],
+        description: "Local bison stewed in thick tomato-walnut sauce",
+        section: "share-hot"
+    },
+    {
+        item_name: "Kupati",
+        item_price: 15,
+        ingredients: [],
+        description: "Pork and beef sausages, with salad of fennel, charred cubanelle and grilled onion, georgian-spiced mustard",
+        section: "share-hot"
+    },
+]
